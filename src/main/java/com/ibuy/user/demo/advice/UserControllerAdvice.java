1bc6409b8e0747dddd601e171a529525a0320922
ajayranam1 <98026013+ajayranam1@users.noreply.github.com>
2022-02-07 17:04:17 +0530

    Update UserControllerAdvice.java

--- src/main/java/com/ibuy/user/demo/advice/UserControllerAdvice.java
+++ src/main/java/com/ibuy/user/demo/advice/UserControllerAdvice.java
@@ -1,5 +1,16 @@
 package com.ibuy.user.demo.advice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ibuy.user.demo.exception.PurchaseItenNotFoundException;
+
 public class UserControllerAdvice {
+
+  @ExceptionHandler(PurchaseItenNotFoundException.class)
+	public ResponseEntity<String> emtyHandler(PurchaseItenNotFoundException purchaseItenNotFoundException){
+		return new ResponseEntity<String>("Invalid Username or Password", HttpStatus.BAD_REQUEST);
+	}
 
 }
